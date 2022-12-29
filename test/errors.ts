bd7735aca648ebb28cee906d75d1596d6e88fb73
dkzlv <dkzlv@pm.me>
2022-12-29 18:11:42 +0000

    Fixing type error messages

--- test/errors.ts
+++ test/errors.ts
@@ -11,9 +11,9 @@ router.subscribe(page => {
   if (!page) {
     console.log('404')
   } else if (page.route === 'post') {
-    // THROWS 'type' does not exist on type 'Params<"id">'
+    // THROWS Property 'type' does not exist on type 'Record<"id", string>'
     router.open(`/post/${page.params.type}`)
-    // THROWS '"exit" | "home" | "create"' and '"creat"' have no overlap
+    // THROWS This condition will always return 'false' since the types '"exit" | "create" | "home"' and '"creat"' have no overlap
   } else if (page.route === 'creat') {
     console.log('create')
   }
@@ -24,7 +24,7 @@ router.subscribe(page => {
   console.log(page.route)
 })
 
-// THROWS 'category' does not exist in type 'Record<"id", string>'.
+// THROWS Argument of type '{ id: string; category: string; }' is not assignable to parameter of type 'Record<"id", string>'.
 openPage(router, 'post', { id: '1', category: 'guides' })
 // THROWS Expected 2 arguments, but got 3
 openPage(router, 'home', { id: '1' })
@@ -34,7 +34,7 @@ openPage(router, 'create', { id: '1' })
 // THROWS Expected 3 arguments, but got 2.
 openPage(router, 'create')
 
-// THROWS 'category' does not exist in type 'Record<"id", string>'.
+// THROWS Argument of type '{ id: string; category: string; }' is not assignable to parameter of type 'Record<"id", string>'.
 redirectPage(router, 'post', { id: '1', category: 'guides' })
 // THROWS Expected 2 arguments, but got 3
 redirectPage(router, 'home', { id: '1' })
